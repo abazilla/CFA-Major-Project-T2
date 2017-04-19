26e139a2ce3f8439a4d25c2e0fb873ddad0d6f66
Nick Wu <nickwu78@gmail.com>
2017-04-19 10:27:21 +1000

    Added home page, and edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'paid_forward/index'
+  root 'home#index'
 
   resources :profiles
   resources :donations
   resources :projects
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end

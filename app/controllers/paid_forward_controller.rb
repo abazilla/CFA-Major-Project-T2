8a139b5274366748fc896958c05f7dd93cd97e95
Nick Wu <nickwu78@gmail.com>
2017-04-21 11:58:46 +1000

    Views built for paid-forward section

--- app/controllers/paid_forward_controller.rb
+++ app/controllers/paid_forward_controller.rb
@@ -7,5 +7,18 @@ class PaidForwardController < ApplicationController
 
   def create
   end
-  
+
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_paid_forward
+      @paid_forward = PaidForward.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def project_params
+      params.require(:paid_forward).permit(:project_id, :amount)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420065430) do
+ActiveRecord::Schema.define(version: 20170421015138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,9 @@ ActiveRecord::Schema.define(version: 20170420065430) do
   create_table "paid_forwards", force: :cascade do |t|
     t.integer  "project_id"
     t.integer  "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.integer  "funded_project"
     t.index ["project_id"], name: "index_paid_forwards_on_project_id", using: :btree
   end
 

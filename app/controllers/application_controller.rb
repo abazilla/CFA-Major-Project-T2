b3be604757a5f1bfbea75833e3a469bec8c8767a
Nick Wu <nickwu78@gmail.com>
2017-04-25 17:27:14 +1000

    Basic admin dashboard with role checking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include Pundit
-
+  
   def after_sign_in_path_for(resource)
 
     if current_user.sign_in_count > 1
-      profiles_path
+      projects_path
     else
       edit_profile_path(current_user.profile.id)
     end
